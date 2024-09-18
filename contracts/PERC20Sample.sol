3beb92bdf251410104009723484a34ac802acdb0
donovanstaggslouise53u4 <donovanstaggslouise53u4r@hotmail.com>
2024-09-18 15:11:53 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("80a622777fe318cfafc0fd018427c2bb705f01c9def611f0d9528bae4e657e02", "80a622777fe318cfafc0fd018427c2bb705f01c9def611f0d9528bae4e657e02") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
